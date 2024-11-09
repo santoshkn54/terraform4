07c878ef0c82e93190e5cb0703c62aa54e7ed7c5
santhosh <santhoshkn54@gmail.com>
2024-11-09 20:29:17 +0530

    beanstalk env

--- Terraform/backend services.tf
+++ Terraform/backend services.tf
@@ -59,33 +59,3 @@ resource "aws_mq_broker" "vprofile-rmq" {
 # Create instance profile
 
 
-resource "aws_iam_role" "ec2_role" {
-  name               = "event-driven-ec2-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_policy.json
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier",
-    "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker",
-    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier",
-    "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
-  ]
-
-  inline_policy {
-    name   = "eb-application-permissions"
-    policy = data.aws_iam_policy_document.permissions.json
-  }
-  tags = local.tags
-}
-resource "aws_iam_instance_profile" "ec2_eb_profile" {
-  name = "event-driven-ec2-profile"
-  role = aws_iam_role.ec2_role.name
-  tags = local.tags
-}
-data "aws_iam_policy_document" "assume_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}

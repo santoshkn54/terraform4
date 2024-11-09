c1ecc68a671a4cbef19e0c806403a0f8c5e6c646
santhosh <santhoshkn54@gmail.com>
2024-11-09 19:44:26 +0530

    beanstalk env

--- Terraform/bean-env.tf
+++ Terraform/bean-env.tf
@@ -12,7 +12,7 @@ resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
-    value     = "arn:aws:iam::545009825113:role/aws-elasticbeanstalk-ec2-role"
+    value     = "arn:aws:iam::545009825113:instance-profile/IamInstanceProfile"
   }
   setting {
     name      = "AssociatePublicIpAddress"

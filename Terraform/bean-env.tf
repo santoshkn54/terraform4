bf8d9646e36fe9ee9107c37b9b2046df3fbf1f08
santhosh <santhoshkn54@gmail.com>
2024-11-09 17:59:48 +0530

    beanstalk env

--- Terraform/bean-env.tf
+++ Terraform/bean-env.tf
@@ -2,7 +2,7 @@ resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
   application = aws_elastic_beanstalk_application.vprofile-prod.name
 
   name                = "vprofile-bean-prod"
-  solution_stack_name = "64bit Amazon Linux 2 v4.7.1 running Tomcat 8.5 Corretto 11"
+  solution_stack_name = "64bit Amazon Linux 2 v4.1.1 running Tomcat 8.5 Corretto 11"
   cname_prefix        = "vprofile-bean-prod-domain"
   setting {
     name      = "VPCId"

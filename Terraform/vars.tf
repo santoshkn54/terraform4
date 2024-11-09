0064aaf8868c2909ff90d9d8af07d03969392b29
santhosh <santhoshkn54@gmail.com>
2024-11-09 16:14:56 +0530

    backend

--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -1,77 +1,77 @@
-variable AWS_REGION {
+variable "AWS_REGION" {
   default = "us-east-1"
 }
-variable AMIS {
-  type = map
+variable "AMIS" {
+  type = map(any)
   default = {
-    us-east-1 = "ami-005fc0f236362e99f"
-    us-east-2 = "ami-005fc0f236362e99f"
+    us-east-1  = "ami-005fc0f236362e99f"
+    us-east-2  = "ami-005fc0f236362e99f"
     ap-south-1 = "ami-005fc0f236362e99f"
   }
 }
-variable PRIV_KEY_PATH {
+variable "PRIV_KEY_PATH" {
   default = "vprofilekey"
 }
-variable PUB_KEY_PATH {
+variable "PUB_KEY_PATH" {
   default = "vprofilekey.pub"
 }
-variable USERNAME {
+variable "USERNAME" {
   default = "ubuntu"
 }
 
-variable MYIP {
+variable "MYIP" {
   default = "49.204.209.185/32"
 }
-variable rmquser {
+variable "rmquser" {
   default = "rabbit"
 }
-variable rmqpass {
+variable "rmqpass" {
   default = "Cool@9366"
 }
-variable dbname {
+variable "dbname" {
   default = "accounts"
 }
-variable dbuser {
+variable "dbuser" {
   default = "admin"
 }
-variable dbpass {
+variable "dbpass" {
   default = "admin123"
 }
-variable instance_count {
+variable "instance_count" {
   default = "1"
 }
-variable VPC_NAME {
+variable "VPC_NAME" {
   default = "vprofile-VPC"
 }
-variable Zone1 {
+variable "Zone1" {
   default = "us-east-1a"
 }
-variable Zone2 {
+variable "Zone2" {
   default = "us-east-1b"
 }
-variable Zone3 {
+variable "Zone3" {
   default = "us-east-1c"
 }
-variable VpcCIDR {
+variable "VpcCIDR" {
   default = "172.21.0.0/16"
 }
-variable PubSub1CIDR {
+variable "PubSub1CIDR" {
   default = "172.21.1.0/24"
 }
-variable PubSub2CIDR {
+variable "PubSub2CIDR" {
   default = "172.21.2.0/24"
 }
-variable PubSub3CIDR {
+variable "PubSub3CIDR" {
   default = "172.21.3.0/24"
 }
 
-variable PrivSub1CIDR {
+variable "PrivSub1CIDR" {
   default = "172.21.4.0/24"
 }
-variable PrivSub2CIDR {
+variable "PrivSub2CIDR" {
   default = "172.21.5.0/24"
 }
-variable PrivSub3CIDR {
+variable "PrivSub3CIDR" {
   default = "172.21.6.0/24"
 }
 

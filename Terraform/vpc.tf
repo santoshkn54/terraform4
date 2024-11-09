30560c9b970449377091128917e22f95efd8193c
santhosh <santhoshkn54@gmail.com>
2024-11-09 15:08:17 +0530

    vpc

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -6,8 +6,8 @@ module "vpc" {
   private_subnets = [var.PrivSub1CIDR,var.PrivSub2CIDR,var.PrivSub3CIDR]
   public_subnets  = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
   enable_nat_gateway = true
-  single_nat_gatway = true
-  enable_dns_hostname = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {

00d50a89ef0e8757f761bca01cd0aaf7f29bc2d6
santhosh <santhoshkn54@gmail.com>
2024-11-09 17:57:42 +0530

    beanstalk env

--- Terraform/secgrp.tf
+++ Terraform/secgrp.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "vprofile-bean-elb-sg" {
   vpc_id      = module.vpc.vpc_id
   egress {
     from_port   = 0
-    protocol    = ""
+    protocol    = "-1"
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }

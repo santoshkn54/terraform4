c64a5f023e506de2d291ad16c97f877ab3c15e18
santhosh <santhoshkn54@gmail.com>
2024-11-10 10:25:39 +0530

    beanstalk env

--- Terraform/backend services.tf
+++ Terraform/backend services.tf
@@ -12,11 +12,11 @@ resource "aws_elasticache_subnet_group" "vprofile-ecache-subgrp" {
 
 }
 resource "aws_db_instance" "vprofile-rds" {
-  instance_class         = "db.t2.micro"
+  instance_class         = "db.m5.large"
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "mysql"
-  engine_version         = "5.7"
+  engine_version         = "5.7.44"
   db_name                = var.dbname
   username               = var.dbuser
   password               = var.dbpass
@@ -46,8 +46,9 @@ resource "aws_mq_broker" "vprofile-rmq" {
   engine_version     = "5.18"
   host_instance_type = "mq.t2.micro"
   security_groups    = [aws_security_group.vprofile-backend-sg.id]
-  subnet_ids         = [module.vpc.private_subnets[0],module.vpc.private_subnets[1]]
-  deployment_mode        = "SINGLE_INSTANCE"
+  subnet_ids         = [module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
+  deployment_mode    = "SINGLE_INSTANCE"
+  auto_minor_version_upgrade = true
 
   user {
 
@@ -59,8 +60,8 @@ resource "aws_mq_broker" "vprofile-rmq" {
 }
 resource "aws_launch_template" "launchtemplate" {
   name          = "example-launch-template"
-  image_id      = "ami-00eb69d236edcfaf8"  # Replace with your AMI ID
-  instance_type = "t2.micro"
+  image_id      = "ami-00eb69d236edcfaf8" # Replace with your AMI ID
+  instance_type = "t3.small"
 
   # Optional: Other settings like security groups, IAM roles, etc.
   key_name = aws_key_pair.vprofilekey.key_name
@@ -68,10 +69,10 @@ resource "aws_launch_template" "launchtemplate" {
 
 # Define your Auto Scaling Group using Launch Template
 resource "aws_autoscaling_group" "autoscalinglaunchtemplate" {
-  desired_capacity     = 2
-  min_size             = 1
-  max_size             = 3
-  vpc_zone_identifier  = [module.vpc.public_subnets[0], module.vpc.public_subnets[1], module.vpc.public_subnets[2]]  # Replace with your subnets
+  desired_capacity    = 2
+  min_size            = 1
+  max_size            = 3
+  vpc_zone_identifier = [module.vpc.public_subnets[0], module.vpc.public_subnets[1], module.vpc.public_subnets[2]] # Replace with your subnets
   launch_template {
     id      = aws_launch_template.launchtemplate.id
     version = "$Latest"

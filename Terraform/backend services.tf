767b162bd246c1f7a7af7a72687cd2ea039ebef4
santhosh <santhoshkn54@gmail.com>
2024-11-10 09:56:45 +0530

    beanstalk env

--- Terraform/backend services.tf
+++ Terraform/backend services.tf
@@ -43,7 +43,7 @@ resource "aws_elasticache_cluster" "vprofile-cache" {
 resource "aws_mq_broker" "vprofile-rmq" {
   broker_name        = "vprofile-rmq"
   engine_type        = "ActiveMQ"
-  engine_version     = "5.16.7"
+  engine_version     = "5.18"
   host_instance_type = "mq.t2.micro"
   security_groups    = [aws_security_group.vprofile-backend-sg.id]
   subnet_ids         = [module.vpc.private_subnets[0],module.vpc.private_subnets[1]]

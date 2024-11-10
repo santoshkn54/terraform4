887be60e37c55abe8daf9451c8f6d250ffb1dafb
santhosh <santhoshkn54@gmail.com>
2024-11-10 10:31:03 +0530

    beanstalk env

--- Terraform/backend services.tf
+++ Terraform/backend services.tf
@@ -15,7 +15,7 @@ resource "aws_db_instance" "vprofile-rds" {
   instance_class         = "db.m5.large"
   allocated_storage      = 20
   storage_type           = "gp2"
-  engine                 = "mysql"
+  engine                 = "MySQL"
   engine_version         = "5.7.44"
   db_name                = var.dbname
   username               = var.dbuser
@@ -46,7 +46,7 @@ resource "aws_mq_broker" "vprofile-rmq" {
   engine_version     = "5.18"
   host_instance_type = "mq.t2.micro"
   security_groups    = [aws_security_group.vprofile-backend-sg.id]
-  subnet_ids         = [module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
+  subnet_ids         = [module.vpc.private_subnets[0]]
   deployment_mode    = "SINGLE_INSTANCE"
   auto_minor_version_upgrade = true
 
@@ -61,7 +61,7 @@ resource "aws_mq_broker" "vprofile-rmq" {
 resource "aws_launch_template" "launchtemplate" {
   name          = "example-launch-template"
   image_id      = "ami-00eb69d236edcfaf8" # Replace with your AMI ID
-  instance_type = "t3.small"
+  instance_type = "t2.micro"
 
   # Optional: Other settings like security groups, IAM roles, etc.
   key_name = aws_key_pair.vprofilekey.key_name

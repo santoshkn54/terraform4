088d671ddec34388d47aefad417a9a3b5f87cf82
santhosh <santhoshkn54@gmail.com>
2024-11-10 09:13:16 +0530

    beanstalk env

--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -1,5 +1,5 @@
 variable "AWS_REGION" {
-  default = "us-east-1"
+  default = "us-east-2"
 }
 variable "AMIS" {
   type = map(any)
@@ -44,13 +44,13 @@ variable "VPC_NAME" {
   default = "vprofile-VPC"
 }
 variable "Zone1" {
-  default = "us-east-1a"
+  default = "us-east-2a"
 }
 variable "Zone2" {
-  default = "us-east-1b"
+  default = "us-east-2b"
 }
 variable "Zone3" {
-  default = "us-east-1c"
+  default = "us-east-2c"
 }
 variable "VpcCIDR" {
   default = "172.21.0.0/16"
